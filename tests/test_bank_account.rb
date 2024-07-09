3dc28edb37d8ffc72a0ab272e73ce2e5c6022b50
moldalievazimbek <148510455+moldalievazim@users.noreply.github.com>
2024-07-09 08:58:06 +0000

    added more tests

--- tests/test_bank_account.rb
+++ tests/test_bank_account.rb
@@ -27,4 +27,52 @@ class TestBankAccount < Minitest::Test
   ensure
     $stdin = STDIN
   end
+
+  def test_withdraw_valid_amount
+    @account.balance = 500
+    input = StringIO.new("200\n")
+    $stdin = input
+
+    @account.withdraw
+
+    assert_equal 300, @account.balance, 'Failed'
+  ensure
+    $stdin = STDIN 
+  end
+
+  def test_withdraw_exceeds_balance
+    @account.balance = 300
+    input = StringIO.new("400\n100\n")
+    $stdin = input
+
+    @account.withdraw
+
+    assert_equal 200, @account.balance, 'Failed'
+  ensure
+    $stdin = STDIN 
+  end
+
+  def test_withdraw_exceeds_limit
+    @account.balance = 1500
+    input = StringIO.new("1200\n800\n")
+    $stdin = input
+
+    @account.withdraw
+
+    assert_equal 700, @account.balance, 'Failed'
+  ensure
+    $stdin = STDIN 
+  end
+
+  def test_withdraw_from_zero_balance
+    @account.balance = 0
+    input = StringIO.new("100\n")
+    $stdin = input
+
+    @account.withdraw
+
+    assert_equal 0, @account.balance, 'Failed'
+  ensure
+    $stdin = STDIN 
+  end
 end
